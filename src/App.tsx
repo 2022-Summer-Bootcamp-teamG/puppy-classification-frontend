2c18542f7ea73177b291aca4076fc25e62dd6ab9
rabbit-22 <chlwlal20@gmail.com>
2022-07-12 14:35:00 +0900

    feat: set router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div className="App">
-        React
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
