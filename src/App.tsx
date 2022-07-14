8cb8cca8260cb0bd842e3f73e8fe776c7b0a619a
Jimi Choi <77673029+rabbit-22@users.noreply.github.com>
2022-07-14 18:00:31 +0900

    Revert "feat: Feature/main template"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import ResultPage from './pages/ResultPage';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/result" element={<ResultPage />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+        React
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
