541f33c861e13163776da8515f0d1745de9f540f
hyelin <k1h2fls@gmail.com>
2022-07-14 21:50:06 +0900

    feat: add DetailPage

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Title } from '../components/common/Title';
 import styled from 'styled-components';
 import { DetailImage } from '../components/common/DetailImage';
+import Header from '../components/Header';
+import sample1 from '../assets/images/sample1.jpg';
+import FeatureBox from '../components/FeatureBox';
+import ChartBox from '../components/common/ChartBox';
 
 /*
     결과 상세 페이지
@@ -15,10 +19,17 @@ import { DetailImage } from '../components/common/DetailImage';
 
 function DetailPage() {
 	return (
-		<Common>
-			<Title>골든리트리버</Title>
-			<DetailImage></DetailImage>
-		</Common>
+		<React.Fragment>
+			<Header />
+			<Common>
+				<Title>골든리트리버</Title>
+				<Box>
+					<DetailImage src={sample1} />
+					<ChartBox />
+				</Box>
+				<FeatureBox content="활발하고 거칠며, 외루움을 잘 타고 어리광을 부린다. 집단 생활에 익숙해져 있어 사교적이고 낯가리지 않는다. 비글의 수명은 15년 정도이고, 주요 질병은 허리디스크, 백내장, 망막 위축 등 눈 질환에 걸리기 쉬운 견종이다." />
+			</Common>
+		</React.Fragment>
 	);
 }
 
@@ -26,10 +37,28 @@ const Common = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	margin-top: 7em;
+	justify-content: center;
+	margin-top: 3rem;
+	@media all and (min-width: 768px) and (max-width: 1023px) {
+		font-size: 0.9rem;
+	}
 	@media all and (max-width: 767px) {
-		margin-top: 4em;
+		font-size: 0.9rem;
 	}
 `;
 
+const Box = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	@media all and (min-width: 768px) and (max-width: 1023px) {
+		align-items: center;
+	}
+	@media all and (max-width: 767px) {
+		flex-direction: column;
+		align-items: center;
+	}
+`;
+
+// const;
 export default DetailPage;
