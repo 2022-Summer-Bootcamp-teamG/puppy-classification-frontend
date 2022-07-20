9207990efa9f5edf213dcd88b4f7a4782135ef03
hyelin <k1h2fls@gmail.com>
2022-07-20 17:12:34 +0900

    feat: add List-page

---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import Header from '../components/common/Header';
+import Categories from '../components/list/Categories';
+import CardList from '../components/list/CardList';
+
+const ListPage = () => {
+  // const [category, setCategory] = useState('all');
+  // const onSelect = useCallback(category => setCategory(category), []);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Categories />
+      <CardList />
+    </React.Fragment>
+  );
+};
+
+export default ListPage;
