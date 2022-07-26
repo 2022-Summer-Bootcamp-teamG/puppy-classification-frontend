bc144db2ee55ae2a449b2f7b7f222bf8fb51935b
hyelin <k1h2fls@gmail.com>
2022-07-26 15:18:03 +0900

    style: add box-shadow in image

--- src/components/list/CardItem.tsx
+++ src/components/list/CardItem.tsx
@@ -22,11 +22,12 @@ function CardItem({ img, breedName }: CardItemProps) {
 const Common = styled.div`
   display: flex;
   flex-direction: column;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 8px 24px,
+    rgba(17, 17, 26, 0.1) 0px 16px 48px;
 `;
 
 const BreedName = styled.div`
-  margin-top: 1rem;
-  margin: auto;
+  margin: 1rem auto;
   font-size: 1.2rem;
   font-family: Pretendard;
   font-weight: 500;
