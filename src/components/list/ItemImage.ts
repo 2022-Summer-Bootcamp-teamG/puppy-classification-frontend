f34d419049e2d83cb87caae91b3b01260d71d400
hyelin <k1h2fls@gmail.com>
2022-08-01 09:41:51 +0900

    fix: image size

--- src/components/list/ItemImage.ts
+++ src/components/list/ItemImage.ts
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 
 const ItemImage = styled.img`
   width: 14rem;
+  height: 12rem;
   display: flex;
 
   margin: 1rem;
   border-radius: 5%;
   @media all and (min-width: 768px) and (max-width: 1023px) {
-    width: 12rem;
   }
   @media all and (max-width: 767px) {
-    width: 18rem;
-    height: 12rem;
+    width: 17rem;
+    height: 14rem;
     margin-bottom: 0.3rem;
   }
 `;
