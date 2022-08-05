04294ad95aa34ce20113b0965c0a5470f268edcd
hyelin <k1h2fls@gmail.com>
2022-08-05 14:58:03 +0900

    fix: chart container size

--- src/components/detail/ChartBox.tsx
+++ src/components/detail/ChartBox.tsx
@@ -116,8 +116,8 @@ const Text = styled.div`
 `;
 
 const Container = styled.div`
-  width: 23rem;
-  margin-left: 3rem;
+  width: 25rem;
+  margin: 0rem auto;
   @media all and (min-width: 768px) and (max-width: 1023px) {
     width: 19rem;
     margin-left: 1rem;
