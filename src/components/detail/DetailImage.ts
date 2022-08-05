1f330be7b7c784a3564ec009cee5ea1dd6a7a148
hyelin <k1h2fls@gmail.com>
2022-08-05 17:32:05 +0900

    style: add box-shadow in image

--- src/components/detail/DetailImage.ts
+++ src/components/detail/DetailImage.ts
@@ -5,6 +5,8 @@ const DetailImage = styled.img`
   height: auto;
   display: flex;
   border-radius: 5%;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px,
+    rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
   @media all and (min-width: 768px) and (max-width: 1023px) {
     width: 18rem;
     height: 14rem;
